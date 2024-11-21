a7f11c4ccecb1240542391c38af78806bba1126c
Gabriel Keith <gabe.keith@yahoo.com>
2024-11-21 18:16:06 -0500

    started enviroment wrapper

---
+++ jaxrl/envs/envpool.py
@@ -0,0 +1,49 @@
+import envpool
+import gymnasium as gym
+import ale_py
+import matplotlib.pyplot as plt
+
+from jax import numpy as jnp
+
+
+class State:
+    def __init__(self):
+        pass
+
+
+class Transition:
+    def __init__(self):
+        pass
+
+
+class Action:
+    def __init__(self):
+        pass
+
+
+class EnvPoolWrapper:
+    def __init__(self):
+        self.is_jittable = False
+    
+    def reset(self) -> tuple[State, Transition]:
+        return State(), Transition()
+
+    def step(self, state: State, action: Transition) -> tuple[State, Transition]:
+        return state, Transition()
+
+
+def main():
+    env = envpool.make_gymnasium("Pong-v5", num_envs=4)
+    # obs, info = env.reset()
+
+
+
+def compare():
+    gym.register_envs(ale_py)
+    env = gym.make_vec("ALE/Pong-v5", num_envs=4)
+    obs, info = env.reset()
+    print(obs.shape)
+
+if __name__ == '__main__':
+    main()
+    # compare()
--- jaxrl/logger.py
+++ jaxrl/logger.py
@@ -1,40 +1,29 @@
-from dataclasses import dataclass
-import logging
 from abc import ABC, abstractmethod
+import os
 from pathlib import Path
+import csv
 
 import jax
+from flax import nnx
 import numpy as np
+from pydantic import TypeAdapter
 from tensorboardX import SummaryWriter
+from sentiment_lm.training_settings import ExperimentSettings
+from sentiment_lm.util import json_normalize
 
-from jaxrl.types import Metrics
-from jaxrl.util import json_normalize
+import neptune
+from neptune.utils import stringify_unsupported
 
-
-@dataclass
-class LoggerConfig:
-    use_console: bool = True
-    use_tb: bool = False
-    use_json: bool = False
-
-
-# Based of mava logger https://github.com/instadeepai/Mava/blob/develop/mava/utils/logger.py
+Metrics = dict[str, jax.Array | nnx.Metric]
 
 
 class BaseLogger(ABC):
     @abstractmethod
-    def __init__(self, cfg: LoggerConfig, unique_token: str):
+    def __init__(self, cfg: ExperimentSettings, unique_token: str):
         pass
 
-    @abstractmethod
-    def log_stat(self, key: str, value: float, step: int) -> None:
+    def log_dict(self, data: Metrics, step: int, event_type: str | None = None) -> None:
         pass
-
-    def log_dict(self, data: Metrics, step: int) -> None:
-        data = json_normalize(data, sep="/")
-
-        for key, value in data.items():
-            self.log_stat(key, value, step)
 
     def close(self) -> None:
         pass
@@ -44,13 +33,9 @@ class MultiLogger(BaseLogger):
     def __init__(self, loggers: list[BaseLogger]) -> None:
         self.loggers = loggers
 
-    def log_stat(self, key: str, value: float, step: int) -> None:
+    def log_dict(self, data: dict, step: int, event_type: str | None = None) -> None:
         for logger in self.loggers:
-            logger.log_stat(key, value, step)
-
-    def log_dict(self, data: dict, step: int) -> None:
-        for logger in self.loggers:
-            logger.log_dict(data, step)
+            logger.log_dict(data, step, event_type)
 
     def close(self) -> None:
         for logger in self.loggers:
@@ -58,49 +43,58 @@ class MultiLogger(BaseLogger):
 
 
 class JaxLogger:
-    """
-    Wrapper for jax dictionaries
-    """
-
-    def __init__(self, cfg: LoggerConfig, unique_token: str):
+    def __init__(self, cfg: ExperimentSettings, unique_token: str):
         loggers: list[BaseLogger] = []
 
-        if cfg.use_tb:
+        if cfg.logger.use_tb:
             loggers.append(TensorboardLogger(cfg, unique_token))
-        if cfg.use_console:
+        if cfg.logger.use_console:
             loggers.append(ConsoleLogger(cfg, unique_token))
-        if cfg.use_json:
-            loggers.append(JsonLogger(cfg, unique_token))
+        if cfg.logger.use_csv:
+            loggers.append(CSVLogger(cfg, unique_token))
+        if cfg.logger.use_neptune:
+            loggers.append(NeptuneLogger(cfg, unique_token))
+        if cfg.logger.use_wandb:
+            loggers.append(WandbLogger(cfg, unique_token))
 
         self.logger = MultiLogger(loggers)
 
-    def log(self, metrics: Metrics, step: int) -> None:
+    def log(self, metrics: Metrics, step: int, event_type: str | None = None) -> None:
         metrics = jax.tree.map(describe, metrics)
-        # metrics = jax.tree.map(lambda x: x.item(), metrics)
-        self.logger.log_dict(metrics, step)
+        self.logger.log_dict(metrics, step, event_type)
+
+    def close(self) -> None:
+        self.logger.close()
 
 
 class TensorboardLogger(BaseLogger):
-    def __init__(self, cfg: LoggerConfig, unique_token: str) -> None:
-        log_path = Path("./tensorboard") / unique_token
+    def __init__(self, cfg: ExperimentSettings, unique_token: str) -> None:
+        log_path = Path("./logs/tensorboard") / unique_token
         self.writer = SummaryWriter(log_path.as_posix())
 
-    def log_stat(self, key: str, value: float, step: int) -> None:
-        self.writer.add_scalar(key, value, step)
+        # flattened = flattened_settings(cfg)
+        # self.writer.add_hparams(hparam_dict=flattened)
+
+    def log_dict(self, data: Metrics, step: int, event_type: str | None = None) -> None:
+        data = json_normalize(data, sep="/")
+        if event_type is not None:
+            data = {f"{event_type}/{k}": v for k, v in data.items()}
+
+        for key, value in data.items():
+            self.writer.add_scalar(key, value, step)
 
     def close(self) -> None:
         self.writer.close()
 
 
 class ConsoleLogger(BaseLogger):
-    def __init__(self, cfg: LoggerConfig, unique_token: str) -> None:
+    def __init__(self, cfg: ExperimentSettings, unique_token: str) -> None:
         pass
 
-    def log_stat(self, key: str, value: float, step: int) -> None:
-        print(f"{key}: {value:.3f}")
-
-    def log_dict(self, data: Metrics, step: int) -> None:
+    def log_dict(self, data: Metrics, step: int, event_type: str | None = None) -> None:
         data = json_normalize(data, sep=" ")
+        if event_type is not None:
+            data = {f"{event_type}/{k}": v for k, v in data.items()}
 
         keys = data.keys()
         values = []
@@ -110,22 +104,63 @@ class ConsoleLogger(BaseLogger):
             values.append(f"{value:.3f}" if isinstance(value, float) else value)
 
         log_str = " | ".join([f"{key}: {value}" for key, value in zip(keys, values)])
+        log_str = f"step: {step} | {log_str}"
         print(log_str)
 
 
-class JsonLogger(BaseLogger):
-    def __init__(self, cfg: LoggerConfig, unique_token: str) -> None:
-        pass
+class CSVLogger(BaseLogger):
+    csv_writer: csv.DictWriter | None = None
 
-    def log_stat(self, key: str, value: float, step: int) -> None:
-        pass
+    def __init__(self, cfg: ExperimentSettings, unique_token: str) -> None:
+        directory = Path("./logs/csv")
+        os.makedirs(directory, exist_ok=True)
 
-    def log_dict(self, data: Metrics, step: int) -> None:
-        pass
+        file = directory / (unique_token + ".csv")
+        self.writer = open(file, "w")
 
+    def log_dict(self, data: Metrics, step: int, event_type: str | None = None) -> None:
+        if event_type is not None:
+            data = {f"{event_type}/{k}": v for k, v in data.items()}
+
+        if self.csv_writer is None:
+            headers = ["step"] + list(data.keys())
+            self.csv_writer = csv.DictWriter(self.writer, fieldnames=headers)
+            self.csv_writer.writeheader()
+
+        row = {"step": step}
+        row.update(data)
+
+        self.csv_writer.writerow(row)
+
+    def close(self) -> None:
+        self.writer.close()
+
+
+class NeptuneLogger(BaseLogger):
+    def __init__(self, cfg: ExperimentSettings, unique_token: str):
+        self.logger = neptune.init_run(
+            project="gabe00122/sentiment-lm",
+        )
+
+        self.logger["config"] = dump_settings(cfg)
+
+    def log_dict(self, data: Metrics, step: int, event_type: str | None = None) -> None:
+        data = json_normalize(data, sep="/")
+        if event_type is not None:
+            data = {f"{event_type}/{k}": v for k, v in data.items()}
+
+        for key, value in data.items():
+            self.logger[key].log(value, step=step)
+
+    def close(self) -> None:
+        self.logger.stop()
 
 def describe(x: dict) -> dict:
     if not isinstance(x, jax.Array) or x.size <= 1:
         return x
 
     return {"mean": np.mean(x), "std": np.std(x), "min": np.min(x), "max": np.max(x)}
+
+
+def dump_settings(settings: ExperimentSettings):
+    return stringify_unsupported(TypeAdapter(ExperimentSettings).dump_python(settings))
