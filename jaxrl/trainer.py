90240e5bcf61228922e89082da4e390fb801524c
Gabriel Keith <gabe.keith@yahoo.com>
2024-12-01 19:15:09 -0500

    training code

--- jaxrl/trainer.py
+++ jaxrl/trainer.py
@@ -1,21 +1,29 @@
 from jax import numpy as jnp
 from flax import nnx
 
+from jaxrl.checkpointer import Checkpointer
 from jaxrl.experiment import Experiment
-from jaxrl.envs.wrapper import EnvWrapper
+from jaxrl.envs.wrapper import EnvWrapper, StepType, TimeStep
 from jaxrl.envs.envpool import EnvPoolWrapper
-from jaxrl.model import create_learner, create_mlp_model
+from jaxrl.logger import JaxLogger
+from jaxrl.model import create_learner
+from jaxrl.systems.actor_critic import ActorCriticLearner
+from jaxrl.systems.types import Transition
+from jaxrl.types import Action, Observation
 
 
-class Trainer:
-    pass
+@nnx.jit
+def act(learner: ActorCriticLearner, observation: Observation, rngs: nnx.Rngs) -> Action:
+    return learner.act(observation, rngs)
+
+@nnx.jit
+def learn(learner: ActorCriticLearner, transition: Transition, rngs: nnx.Rngs):
+    learner.learn(transition, rngs)
 
 
 def train(experiment: Experiment):
     print("Training...")
     print(experiment.config)
-
-    # return
 
     environment: EnvWrapper = EnvPoolWrapper(
         experiment.config.environment.name,
@@ -23,14 +31,17 @@ def train(experiment: Experiment):
         experiment.environments_seed,
     )
 
+    logger = JaxLogger(experiment.config, experiment.unique_token)
+    checkpointer = Checkpointer(experiment.checkpoints_dir)
+
     print(environment.action_spec)
     print(environment.observation_spec)
 
-    state, time_step = environment.reset()
-    # state, next_time_step = environment.step(state, jnp.zeros(environment.num_envs, dtype=jnp.int32))
-    # # print(next_time_step)
-
-    rngs = nnx.Rngs(params=experiment.params_seed, action=experiment.actions_seed)
+    rngs = nnx.Rngs(
+        default=experiment.default_seed,
+        params=experiment.params_seed,
+        action=experiment.actions_seed,
+    )
 
     learner = create_learner(
         experiment.config.learner,
@@ -40,6 +51,35 @@ def train(experiment: Experiment):
         rngs=rngs,
     )
 
-    actions = learner.act(time_step.observation, rngs)
-    print(actions)
-    # print(learner)
+    state, time_step = environment.reset()
+
+    for global_step in range(experiment.config.environment.max_steps):
+        action = act(learner, time_step.observation, rngs)
+        state, next_time_step = environment.step(state, action)
+
+        transition = make_transition(time_step, action, next_time_step)
+
+        learn(learner, transition, rngs)
+
+        time_step = next_time_step
+
+        if global_step % experiment.config.logger.log_rate == experiment.config.logger.log_rate - 1:
+            logger.log(learner.metrics.compute(), global_step)
+            learner.metrics.reset()
+
+            checkpointer.save(learner, global_step)
+
+    checkpointer.save(learner, experiment.config.environment.max_steps)
+    checkpointer.close()
+    logger.close()
+
+
+def make_transition(time_step: TimeStep, action: Action, next_time_step: TimeStep) -> Transition:
+    return Transition(
+        observation=time_step.observation,
+        action=action,
+        reward=next_time_step.reward,
+        next_observation=next_time_step.observation,
+        terminated=next_time_step.step_type == StepType.LAST,
+        truncated=jnp.bool(False),
+    ) 
