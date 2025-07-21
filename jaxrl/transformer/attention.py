240ef32d0bc0144313afc0f41cd7e2d60e2f1ccd
Gabriel Keith <gfk1995@gmail.com>
2025-07-21 17:36:53 -0400

    cudnn

--- jaxrl/transformer/attention.py
+++ jaxrl/transformer/attention.py
@@ -8,10 +8,6 @@ from jaxrl.transformer import positional_embeddings
 from jaxrl.constants import index_type
 
 
-def softcap(x, cap):
-    return jnp.tanh(x / cap) * cap
-
-
 def position_mask(time_steps, max_seq_length: int):
     seq_range = jnp.arange(max_seq_length, dtype=index_type)
     mask = seq_range[None, None, :] <= time_steps[:, :, None]
@@ -19,7 +15,7 @@ def position_mask(time_steps, max_seq_length: int):
 
 
 def einsum_attention(
-    query, key, value, mask, *, attention_softcap: float | None = None
+    query, key, value, mask
 ):
     dtype = query.dtype
 
@@ -30,9 +26,6 @@ def einsum_attention(
 
     big_neg = jnp.finfo(dtype).min
     attn_weights = jnp.where(mask, attn_weights, big_neg)
-
-    if attention_softcap is not None:
-        attn_weights = softcap(attn_weights, attention_softcap)
 
     attn_weights = jax.nn.softmax(attn_weights, axis=-1).astype(key.dtype)
 
@@ -48,33 +41,39 @@ class KVCache(NamedTuple):
 class AttentionBlock(nnx.Module):
     def __init__(
         self,
-        num_heads: int,
         d_model: int,
+        head_dim: int,
+        num_heads: int,
+        num_kv_heads: int,
         *,
+        use_built_in: bool = True,
         max_seq_length: int,
-        rope_max_wavelength: float,
+        rope_max_wavelength: float = 10_000,
+        use_qk_norm: bool = False,
         attention_softcap: float | None = None,
         dtype: DTypeLike | None = None,
         param_dtype: DTypeLike = jnp.float32,
         kernel_init: nnx.Initializer = nnx.initializers.normal(),
         rngs: nnx.Rngs,
     ):
-        self.num_heads = num_heads
-        self.num_kv_heads = 1
         self.d_model = d_model
+        self.head_dim = head_dim
+        self.num_heads = num_heads
+        self.num_kv_heads = num_kv_heads
+        self.use_qk_norm = use_qk_norm
         self.max_seq_length = max_seq_length
         self.rope_max_wavelength = rope_max_wavelength
         self.attention_softcap = attention_softcap
         self.dtype = dtype
         self.param_dtype = param_dtype
 
+        self.use_built_in = use_built_in
+
         if self.d_model % self.num_heads != 0:
             raise ValueError(
                 f"Memory dimension ({self.d_model}) must be divisible by "
                 f"'num_heads' heads ({self.num_heads})."
             )
-
-        self.head_dim = self.d_model // self.num_heads
 
         self.kv_proj = nnx.LinearGeneral(
             in_features=self.d_model,
@@ -104,27 +103,23 @@ class AttentionBlock(nnx.Module):
             rngs=rngs,
         )
 
-        self._query_norm = nnx.RMSNorm(self.head_dim, rngs=rngs)
-        self._key_norm = nnx.RMSNorm(self.head_dim, rngs=rngs)
+        if self.use_qk_norm:
+            self._query_norm = nnx.RMSNorm(self.head_dim, rngs=rngs)
+            self._key_norm = nnx.RMSNorm(self.head_dim, rngs=rngs)
 
     def create_kv_cache(
-        self, batch_size: int, context_size: int, *, dtype: DTypeLike | None = None
+        self, batch_size: int
     ) -> KVCache:
-        shape = (batch_size, context_size, self.num_kv_heads, self.head_dim)
-        key = jnp.zeros(shape, dtype=dtype)
-        value = jnp.zeros(shape, dtype=dtype)
+        shape = (batch_size, self.max_seq_length, self.num_kv_heads, self.head_dim)
+        key = jnp.zeros(shape, dtype=self.dtype)
+        value = jnp.zeros(shape, dtype=self.dtype)
         return KVCache(key, value)
 
     def update_kv_cache(self, kv_cache: KVCache, seq_pos, key, value) -> KVCache:
-        batch_idx = jnp.arange(kv_cache.key.shape[0], dtype=index_type)
-        batch_idx = batch_idx[:, None]
-        # pos = seq_pos[0, 0]
+        pos = seq_pos[0, 0]
 
-        # todo: investigate dynamic slice?
-        # key = jax.lax.dynamic_update_slice(kv_cache.key, key, (0, pos, 0, 0))
-        # value = jax.lax.dynamic_update_slice(kv_cache.value, value, (0, pos, 0, 0))
-        key = kv_cache.key.at[batch_idx, seq_pos].set(key)
-        value = kv_cache.value.at[batch_idx, seq_pos].set(value)
+        key = jax.lax.dynamic_update_slice(kv_cache.key, key, (0, pos, 0, 0))
+        value = jax.lax.dynamic_update_slice(kv_cache.value, value, (0, pos, 0, 0))
 
         return KVCache(key, value)
 
@@ -133,8 +128,9 @@ class AttentionBlock(nnx.Module):
         key, value = jnp.split(kv_proj, 2, -1)
         query = self.query_proj(inputs)
 
-        # query = self._query_norm(query)
-        # key = self._key_norm(key)
+        if self.use_qk_norm:
+            query = self._query_norm(query)
+            key = self._key_norm(key)
 
         query = positional_embeddings.apply_rope(query, seq_pos, self.head_dim, self.rope_max_wavelength)
         key = positional_embeddings.apply_rope(key, seq_pos, self.head_dim, self.rope_max_wavelength)
@@ -145,15 +141,23 @@ class AttentionBlock(nnx.Module):
             value = kv_cache.value
 
         mask = position_mask(seq_pos, self.max_seq_length)
+        # jax.debug.print("- {}", mask)
 
-        # x = jax.nn.dot_product_attention(query, key, value, implementation='cudnn')
-        x = einsum_attention(
-            query,
-            key,
-            value,
-            mask,
-            attention_softcap=self.attention_softcap,
-        )
+        if self.use_built_in:
+            batch, seq, _ = inputs.shape
+            training = seq > 1
+            if training:
+                x = jax.nn.dot_product_attention(query, key, value, is_causal=True, implementation='cudnn')
+            else:
+                kv_length = jnp.full((batch,), seq_pos[0, 0]+1)
+                x = jax.nn.dot_product_attention(query, key, value, key_value_seq_lengths=kv_length, implementation='cudnn')
+        else:
+            x = einsum_attention(
+                query,
+                key,
+                value,
+                mask,
+            )
 
         out = self.out(x)
 
