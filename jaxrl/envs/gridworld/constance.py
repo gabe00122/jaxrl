79e26e07c1538b8bedccf557d338c69c33c09a91
Gabriel Keith <gfk1995@gmail.com>
2025-09-05 21:05:40 -0400

    small teams

--- jaxrl/envs/gridworld/constance.py
+++ jaxrl/envs/gridworld/constance.py
@@ -1,6 +1,6 @@
 from jax import numpy as jnp
 
-NUM_TYPES = 29
+NUM_TYPES = 14
 
 # Unified tile ids across gridworld environments
 TILE_EMPTY = 0              # empty space
@@ -9,39 +9,39 @@ TILE_DESTRUCTIBLE_WALL = 2  # destructible
 TILE_FLAG = 3               # typical goal tile
 TILE_FLAG_UNLOCKED = 4      # used for the scouting environment where the the flag gets made available for taking
 
-TILE_FLAG_BLUE_TEAM = 5
-TILE_FLAG_RED_TEAM = 6
+# TILE_FLAG_BLUE_TEAM = 5
+# TILE_FLAG_RED_TEAM = 6
 
 # agents are observed like tiles
-AGENT_GENERIC = 7           # typical agent
-AGENT_SCOUT = 8             # scout agent (scout env)
-AGENT_HARVESTER = 9         # harvester agent (scout env)
+AGENT_GENERIC = 5           # typical agent
+AGENT_SCOUT = 6             # scout agent (scout env)
+AGENT_HARVESTER = 7         # harvester agent (scout env)
 
-AGENT_RED_KNIGHT_UP    = 10
-AGENT_RED_KNIGHT_RIGHT = 11
-AGENT_RED_KNIGHT_DOWN  = 12
-AGENT_RED_KNIGHT_LEFT  = 13
+# AGENT_RED_KNIGHT_UP    = 10
+AGENT_RED_KNIGHT_RIGHT = 8
+# AGENT_RED_KNIGHT_DOWN  = 12
+# AGENT_RED_KNIGHT_LEFT  = 13
 
-AGENT_RED_ARCHER_UP    = 14
-AGENT_RED_ARCHER_RIGHT = 15
-AGENT_RED_ARCHER_DOWN  = 16
-AGENT_RED_ARCHER_LEFT  = 17
+# AGENT_RED_ARCHER_UP    = 14
+# AGENT_RED_ARCHER_RIGHT = 15
+# AGENT_RED_ARCHER_DOWN  = 16
+# AGENT_RED_ARCHER_LEFT  = 17
 
-AGENT_BLUE_KNIGHT_UP    = 18
-AGENT_BLUE_KNIGHT_RIGHT = 19
-AGENT_BLUE_KNIGHT_DOWN  = 20
-AGENT_BLUE_KNIGHT_LEFT  = 21
+# AGENT_BLUE_KNIGHT_UP    = 18
+AGENT_BLUE_KNIGHT_RIGHT = 9
+# AGENT_BLUE_KNIGHT_DOWN  = 20
+# AGENT_BLUE_KNIGHT_LEFT  = 21
 
-AGENT_BLUE_ARCHER_UP    = 22
-AGENT_BLUE_ARCHER_RIGHT = 23
-AGENT_BLUE_ARCHER_DOWN  = 24
-AGENT_BLUE_ARCHER_LEFT  = 25
+# AGENT_BLUE_ARCHER_UP    = 22
+# AGENT_BLUE_ARCHER_RIGHT = 23
+# AGENT_BLUE_ARCHER_DOWN  = 24
+# AGENT_BLUE_ARCHER_LEFT  = 25
 
 
-TILE_DECOR_1 = 26
-TILE_DECOR_2 = 27
-TILE_DECOR_3 = 28
-TILE_DECOR_4 = 29
+TILE_DECOR_1 = 10
+TILE_DECOR_2 = 11
+TILE_DECOR_3 = 12
+TILE_DECOR_4 = 13
 
 
 # Actions
